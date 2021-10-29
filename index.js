8d793e99545d945f60165e004feb44adeb0c8f4d
Mahir-Asef <mahirasef30@gmail.com>
2021-10-29 18:00:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.
+						swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected');
+    }
+    finally {
+        // await client.close()
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(Example app listening at http://localhost:${port})
+})
