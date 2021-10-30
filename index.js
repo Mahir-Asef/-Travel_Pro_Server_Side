6501342375869b0fdb22c34936ab8ac3bb8e18dd
Mahir-Asef <mahirasef30@gmail.com>
2021-10-30 23:20:28 +0600

    details added

--- index.js
+++ index.js
@@ -1,38 +1,61 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
-
-const app = express()
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.
-						swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-console.log(uri);
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sd44u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run() {
-    try {
-        await client.connect();
-        console.log('database connected');
-    }
-    finally {
-        // await client.close()
-    }
 
+async function run() {
+  try {
+
+    await client.connect();
+    const database = client.db("travelPro");
+    const tripsCollection = database.collection("trips");
+
+
+    // GET API
+
+
+    app.get("/trips", async (req, res) => {
+      const cursor = tripsCollection.find({});
+      const trips = await cursor.toArray();
+      res.send(trips);
+    });
+
+
+    // GET SINGLE TOUR INFO
+
+
+    app.get("/trips/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("getting specific trip", id);
+      const query = { _id: ObjectId(id) };
+      const trip = await tripsCollection.findOne(query);
+      res.json(trip);
+    });
+  } 
+  finally {
+    // await client.close()
+  }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 
 app.listen(port, () => {
-    console.log(Example app listening at http://localhost:${port})
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
